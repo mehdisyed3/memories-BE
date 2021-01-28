b583440f23f2b71746ff4c18bac8a6b297b8e563
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-28 13:59:34 -0500

    schema created, post method created

---
+++ controllers/post.js
@@ -0,0 +1,17 @@
+import PostMessage from '../models/postsMessage.js'
+
+
+export const getPosts = async (req,res) => {
+  try {
+    const PostMessages = await PostMessage.find();
+
+    res.status(200).json(PostMessages)
+    
+  } catch (error) {
+    res.status(404).json({message:error.message})
+  }
+}
+
+export const createPost = ( req,res )=>{
+  res.send('Post Creation');
+}
