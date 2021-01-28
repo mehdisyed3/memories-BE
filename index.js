4b0723a47bbb2b6999f95fae137d0f7a5eb04d89
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-28 13:33:44 -0500

    dependencies installed. routes set. working on localhost 5000

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/post.js'
+
+
+const app = express();
+
+app.use('/post',postRoutes);
+
+app.use(bodyParser.json({limit:"30mb", extended:"true"}))
+app.use(bodyParser.urlencoded({limit:"30mb", extended:"true"}))
+app.use(cors());
+
+const CONNECTION_URL='mongodb+srv://oni:Onioni22@cluster0.y8usq.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+        .then(()=> app.listen(PORT, ()=> console.log(`Server running on port:${PORT}`)))
+        .catch(err => console.log(err.message))
+
+
+mongoose.set('useFindAndModify', false)
