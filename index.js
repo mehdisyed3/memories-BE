51cd9d16830f4be5d49afe1b728c9ceb281154e3
Syed <mehdisyed3@gmail.com>
2021-01-31 18:45:39 -0500

    clean commit

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ import cors from 'cors';
 
 import postRoutes from './routes/post.js'
 
-
 const app = express();
-
 
 app.use(bodyParser.json({limit:"30mb", extended:"true"}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:"true"}))
@@ -20,6 +18,5 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
         .then(()=> app.listen(PORT, ()=> console.log(`Server running on port:${PORT}`)))
         .catch(err => console.log(err.message))
-
 
 mongoose.set('useFindAndModify', false)
